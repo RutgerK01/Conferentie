84a09a8791dd6feccc62a032f62a9996c0d75bd6
mats <matsstam01@gmail.com>
2019-03-21 09:36:13 +0100

    big update

---
+++ car.php
@@ -0,0 +1,31 @@
+<?php 
+  $cards = "1";
+  $full = false;
+
+  if($cards == 0){
+    $full = true;
+  }
+?>
+
+<div class="row">
+<div class="col-1"></div>
+<div class="col-10" id="car">
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <a href="./about.php">
+        <?php if($full){echo('<img src="./images/Banner1.2.png" class="d-block w-100" alt="...">');} 
+              else{echo('<img src="./images/Banner1.png" class="d-block w-100" alt="...">');}?> </a>
+    </div>
+    <div class="carousel-item">
+    <a href="registerform.php?action=user"><img src="./images/Banner2.png" class="d-block w-100" alt="..."></a>
+    </div>
+    <div class="carousel-item">
+    <a href=""><img src="./images/Banner3.png" class="d-block w-100" alt="..."></a>
+    </div>
+  </div>
+</div>
+
+</div>
+<div class="col-1"></div>
+</div>
